2b50102906ef4fcad0a71634cb912f0a6a434343
Vinsent034 <vincenzo0144@gmail.com>
2026-03-06 19:14:48 +0100

    feat(auth): applica AuthGuard alla rotta issue-list

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,9 +1,10 @@
 import { Routes } from '@angular/router';
 import { Login } from './pages/login/login';
 import { IssueList } from './pages/issue-list/issue-list';
+import { authGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
   { path: 'login', component: Login },
-  { path: 'issue-list', component: IssueList },
+  { path: 'issue-list', component: IssueList, canActivate: [authGuard] },
   { path: '', redirectTo: 'login', pathMatch: 'full' },
 ];
